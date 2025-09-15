381b22ca54667dff77cc2ef48ed4c4ca1aedf9fd
Rean Tsai <aobc55625@gmail.com>
2025-09-15 16:23:13 +0800

    fix db

--- src/config/db.js
+++ src/config/db.js
@@ -6,9 +6,7 @@ dotenv.config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? {
-    rejectUnauthorized: false
-  } : false,
+  ssl: false, // 暫時關閉 SSL
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
